e4ce3d60c87939f27723f3207112f61c6203b0dc
MyAeroCode <meteo_meteo@naver.com>
2020-10-28 10:47:03 +0900

    impl: Project Doc

--- src/react/components/pages/HelpPage/docs/doc.project.tsx
+++ src/react/components/pages/HelpPage/docs/doc.project.tsx
@@ -2,10 +2,14 @@ import React from "react";
 import { Service } from "typedi";
 import { Doc } from ".";
 import { GithubOutlined } from "@ant-design/icons";
+import { stripIndent } from "common-tags";
 
 @Service()
 export class ProjectDoc implements Doc {
     icon = (<GithubOutlined />);
     title = "Project";
-    contents = `Project Doc.`;
+    contents = stripIndent`
+        Author : AeroCode
+        GitHub : https://github.com/MyAeroCode/algo-mocha
+    `;
 }
