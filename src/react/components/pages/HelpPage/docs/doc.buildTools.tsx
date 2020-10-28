f95206612ebac4b67fced1d1263052549851192b
MyAeroCode <meteo_meteo@naver.com>
2020-10-28 10:43:44 +0900

    impl: Build Tools Doc

--- src/react/components/pages/HelpPage/docs/doc.buildTools.tsx
+++ src/react/components/pages/HelpPage/docs/doc.buildTools.tsx
@@ -2,10 +2,27 @@ import React from "react";
 import { Service } from "typedi";
 import { Doc } from ".";
 import { ToolOutlined } from "@ant-design/icons";
+import { stripIndent } from "common-tags";
 
 @Service()
 export class BuildToolsDoc implements Doc {
     icon = (<ToolOutlined />);
     title = "Build Tools";
-    contents = `Build Tools Doc.`;
+    contents = stripIndent`
+        # C
+            compile: gcc -o test-n test-n.cpp
+            execute: test-n
+
+        # C++
+            compile: g++ -o test-n test-n.cpp
+            execute: test-n
+
+        # JavaScript
+            compile: 
+            execute: node test-n.js
+
+        # TypeScript
+            compile: tsc test-n.ts
+            execute: node test-n.js
+    `;
 }
